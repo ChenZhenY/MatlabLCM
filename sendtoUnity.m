851753ffa821ec5e9a95b860cfe330758853b172
Zhenyang Chen <chen-zhenyang@outlook.com>
2022-01-28 18:38:15 -0500

    Arrow Test

--- sendtoUnity.m
+++ sendtoUnity.m
@@ -20,52 +20,26 @@ publisher = lcm.lcm.LCM.getSingleton();
 humanoid_state_msg = lcmtypes.humanoid_state_info_lcmt();
 debug_visual_msg   = lcmtypes.debug_visualization_lcmt();
 
-%% arrow test
+%% arrow msg initialization
 num_arrows = 2;
 debug_visual_msg.arrow_count=num_arrows;
 debug_visual_msg.arrow_elements=num_arrows;
 debug_visual_msg.arrow_position_elements=num_arrows*3;
 debug_visual_msg.arrow_color_elements=num_arrows*4;
+
 debug_visual_msg.arrow_base_positions = zeros(num_arrows*3,1);
 debug_visual_msg.arrow_directions = zeros(num_arrows*3,1);
-% debug_visual_msg.arrow_head_width = zeros(num_arrows,1);
-% debug_visual_msg.arrow_head_length = zeros(num_arrows,1);
-% debug_visual_msg.arrow_shaft_width = zeros(num_arrows,1);
+debug_visual_msg.arrow_nom = [30, 30];
+debug_visual_msg.arrow_ids = ["GRF 1", "GRF 2"]; 
 debug_visual_msg.arrow_colors = zeros(num_arrows*4,1);
 
-%     int32_t arrow_count;
-%     int32_t arrow_elements;
-%     int32_t arrow_position_elements;
-%     int32_t arrow_color_elements;
-%     float arrow_base_positions[arrow_position_elements];
-%     float arrow_directions[arrow_position_elements];
-%     float arrow_head_width[arrow_elements];
-%     float arrow_head_length[arrow_elements];
-%     float arrow_shaft_width[arrow_elements];
-%     float arrow_colors[arrow_color_elements];
-
-%     forceArrow->head_width = 0.0125;
-%     forceArrow->head_length = 0.04;
-%     forceArrow->shaft_width = 0.004;
-%     forceArrow->color={1.0, 0.0, 0.0, 0.7};
-
 for i=1:num_arrows
-    debug_visual_msg.arrow_base_positions(3*(i-1)+1)= 0;
-    debug_visual_msg.arrow_base_positions(3*(i-1)+2)= 0;
-    debug_visual_msg.arrow_base_positions(3*(i-1)+3)= 0;
-    debug_visual_msg.arrow_directions(3*(i-1)+1)= 5*i;
-    debug_visual_msg.arrow_directions(3*(i-1)+2)= 5*i;
-    debug_visual_msg.arrow_directions(3*(i-1)+3)= 5*i;
-%     debug_visual_msg.arrow_head_width(i) = 0.0125;
-%     debug_visual_msg.arrow_head_length(i)= 0.04;
-%     debug_visual_msg.arrow_shaft_width(i)= 0.004;
     debug_visual_msg.arrow_colors(4*(i-1)+1)= 1;
     debug_visual_msg.arrow_colors(4*(i-1)+2)= 0;
     debug_visual_msg.arrow_colors(4*(i-1)+3)= 0;
     debug_visual_msg.arrow_colors(4*(i-1)+4)= 0.7;
 end
 
-publisher.publish('debug_visualization', debug_visual_msg);
 %% Loading and Processing mat.file
 % matfile: x y z roll pitch yaw right(Hipz Hipx Hipy Knee Ankle)7-11 left(Hipz Hipx Hipy Knee Ankle)12-16 rightArm leftArm
 
@@ -90,6 +64,28 @@ for i = 1:t_all
     rotM = rotationMatrix(-rotangle(3),'y')*rotationMatrix(-rotangle(2), 'z')*rotationMatrix(-rotangle(1),'x')*rotationMatrix(pi/2, 'y');
     humanoid_state_msg.body_ori_quat_visual(:) = rotm2quat(rotM);
 
+    debug_visual_msg.arrow_base_positions(1)= i/500;
+    debug_visual_msg.arrow_base_positions(2)= 1;
+    debug_visual_msg.arrow_base_positions(3)= 1;
+    debug_visual_msg.arrow_directions(1) = 30;
+    debug_visual_msg.arrow_directions(2) = 0;
+    debug_visual_msg.arrow_directions(3) = 0;
+%     debug_visual_msg.arrow_base_positions(1)= raw_pos(1);
+%     debug_visual_msg.arrow_base_positions(2)= raw_pos(2);
+%     debug_visual_msg.arrow_base_positions(3)= raw_pos(3);
+%     debug_visual_msg.arrow_directions(1) = raw_pos(1) ;
+%     debug_visual_msg.arrow_directions(2) = raw_pos(2)+ i/10;
+%     debug_visual_msg.arrow_directions(3) = raw_pos(3);
+
+%     debug_visual_msg.arrow_base_positions(3*(i-1)+1)= 0.02*t;
+%     debug_visual_msg.arrow_base_positions(3*(i-1)+2)= 0.03*t;
+%     debug_visual_msg.arrow_base_positions(3*(i-1)+3)=0 ;
+%     debug_visual_msg.arrow_directions(3*(i-1)+1)= t;
+%     debug_visual_msg.arrow_directions(3*(i-1)+2)= t;
+%     debug_visual_msg.arrow_directions(3*(i-1)+3)= t;
+%     debug_visual_msg.arrow_directions(4) = -t;
+
     publisher.publish('humanoid_visualization_info', humanoid_state_msg);
+    publisher.publish('debug_visualization', debug_visual_msg);
     pause(t_step);
 end
